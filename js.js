36f61091b61e6be339a630152dc5160a6daa54ad
Maria Alícia <ma.aliciamenezes@gmail.com>
2024-12-01 14:17:07 -0300

    finalizando a verificação no login

--- js.js
+++ js.js
@@ -1,3 +1,23 @@
 function mudarUsuario(){
    
 }
+function mudarCor1(){
+    iEntrar.style.backgroundColor = "#300098";
+}
+function mudarCor2(){
+    iEntrar.style.backgroundColor = "#4C00F5";
+}
+
+function loginSite(){
+    if(iSenha.value == "Info_2022" && INome.value == "informaticaporamor@gmail.com"){
+     return true;
+    }else{
+        alerta.style.display ="block";
+        document.getElementById("form").reset();
+        return false;
+    }
+ }
+
+ 
+ 
+
