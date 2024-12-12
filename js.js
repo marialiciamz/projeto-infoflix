8998eade18dc6611498feb511bc1b2ac56f46a07
Maria Alícia <ma.aliciamenezes@gmail.com>
2024-12-11 22:00:59 -0300

    alguns comentarios

--- js.js
+++ js.js
@@ -1,18 +1,13 @@
-function mudarUsuario(){
-   if(conta.img == "indexUsuario1.html"){
-    conta.img = "indexUsuario2.html";
-   }else if(conta.img == "indexUsuario2.html"){
-    conta.img = "indexUsuario1.html";
-   }
-}
 
+//muda a cor do botão de envio se caso ele for clicado
 function mudarCor1(){
     iEntrar.style.backgroundColor = "#300098";
 }
+//muda a cor do botão de envio se caso um input estiver selecionado
 function mudarCor2(){
     iEntrar.style.backgroundColor = "#4C00F5";
 }
-
+//verifica se a senha e o email estão corretos, se não estiverem informa uma menssagem de erro
 function loginSite(){
     if(iSenha.value == "Info_2022" && INome.value == "informaticaporamor@gmail.com"){
      return true;
@@ -23,7 +18,7 @@ function loginSite(){
     }
  }
 
-
+// adiciona um evento para mudar a cor de fundo do menu de acordo com a rolagem do scrull
 window.addEventListener('scroll', function(){
     var elementoComMudanca = this.document.querySelector('menu');
     var scrollPosition = this.window.scrollY;
